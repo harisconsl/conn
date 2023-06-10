c0b14303e36fe8b093cad6d8633ea2afd4ac3f9b
Hari <harisconsl@gmail.com>
2023-06-10 16:28:11 +0530

    timer example added

--- timer_loop.cpp
+++ timer_loop.cpp
@@ -1,6 +1,15 @@
-#include <boost/asio.hpp>
-#include <iostream>
 
+#define BOOST_ASIO_ENABLE_HANDLER_TRACKING 1
+
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+//#include <boost_asio/example/cpp11/handler_tracking/custom_tracking.hpp>
+#include <iostream>
+#include <chrono>
+
+
+boost::asio::io_context io_context;
 int factorial(int n)
 {
   int res = 1;
@@ -10,31 +19,45 @@ int factorial(int n)
   return res;
 }
 
-void handleEvent(const boost::system::error_code& error) {
+void handleEvent(const boost::system::error_code& error)
+{
   if (!error)
     {
       // Perform the desired task or operation
       
       // Reschedule the event
       // For example, reschedule it after 1 second
-      std::cout << "I am calculating factorial" << std::endl;
-      factorial(10);
+      std::cout << "I am calculating factorial : " << factorial(10) <<std::endl;
+ 
       boost::asio::steady_timer timer(io_context);
-      timer.expires_after(std::chrono::seconds(1));
+      timer.expires_after(std::chrono::seconds(10));
       timer.async_wait(handleEvent);
     }
+  else
+    std::cout << "Boost::error : " << error.message() <<std::endl;  
 }
 
-int main() {
-    boost::asio::io_context io_context;
+int main()
+{
+  // Schedule the first event
+  boost::asio::steady_timer timer(io_context);
+  timer.expires_after(std::chrono::seconds(1));
 
-    // Schedule the first event
-    boost::asio::steady_timer timer(io_context);
-    timer.expires_after(std::chrono::seconds(1));
-    timer.async_wait(handleEvent);
-
-    // Run the event loop
-    io_context.run();
-
-    return 0;
+  std::function<void(const boost::system::error_code&)> handler;
+  
+  handler = [&](const boost::system::error_code& ec) {
+    if (!ec)
+      {
+	std::cout << "factorial is " << factorial(10) << std::endl;
+	timer.expires_after(std::chrono::seconds(1));
+	timer.async_wait(handler);
+      }
+    else
+      std::cout << "Boost::error " << ec.message() << std::endl;
+  };
+  timer.async_wait(handler);
+  
+  // Run the event loop
+  io_context.run();
+  return 0;
 }
